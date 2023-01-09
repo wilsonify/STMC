9614c0cc29fb18878be8edafde1f8dba3472dba7
wilsonify <tom.andrew.wilson@gmail.com>
2023-01-09 08:18:38 -0500

    iso_c addln

--- src/ForLib/addln_cut.f
+++ src/ForLib/addln_cut.f
@@ -1,8 +1,17 @@
       FUNCTION ADDLN_CUT(ALN,BLN)
-C Copyright Bernd Berg, May 25 2001.
-C Given ln(A) and ln(B), the function returns ln(C) with C=A+B.
-      include 'implicit.sta'
-      include 'constants.par'
+      ! Copyright Bernd Berg, May 25 2001.
+      ! Given ln(A) and ln(B), the function returns ln(C) with C=A+B.
+      ! include 'implicit.sta'
+      ! include 'constants.par'
+      use iso_c_binding          
+      implicit none
+      real(c_double) :: ADDLN_CUT
+      real(c_double) :: ALN
+      real(c_double) :: BLN
+      real(c_double), parameter :: ONE = 1.0_c_double 
+      real(c_double) :: ABS_LN
+      real(c_double) :: CUT_OFF
+      
       PARAMETER(CUT_OFF=700*ONE) ! Change with precision!
       ABS_LN=ABS(ALN-BLN)
       IF(ABS_LN.GT.CUT_OFF) THEN
---
+++ src/ForLib/addln_cut.h
@@ -0,0 +1,4 @@
+extern "C"
+{
+    double ADDLN_CUT(double ALN, double BLN);
+}
